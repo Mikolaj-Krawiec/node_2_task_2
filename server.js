55e201a609315b80a3deba7b75f02a86b4f4ac02
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-08-07 18:17:17 +0200

    Task_3

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
@@ -31,6 +33,19 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, file } = req.body;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file});
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
 });
 
 app.use((req, res) => {
